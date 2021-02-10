0d55b98b682d556c78bfa8cf2dd3659ef7eb3c32
Nandi1218 <kanyicskan@gmail.com>
2021-02-10 14:44:50 +0100

    Create game.cs

---
+++ Projektmunka/Projekt/Games/game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class game
+    {
+        public game()
+        {
+
+        }
+        string name;
+        int releaseDate;
+        int price;
+        int rateing;
+        public string Name { get; set; }
+        public int ReleaseDate { get; set; }
+        public int Price { get; set; }
+        public int Rateing { get; set; }
+    }
+}
