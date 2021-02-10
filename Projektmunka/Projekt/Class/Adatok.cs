ba6600ac287a1d07c99f3548270ea37bce30810a
Nandi1218 <kanyicskan@gmail.com>
2021-02-10 14:44:43 +0100

    Create Adatok.cs

---
+++ Projektmunka/Projekt/Class/Adatok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class Adatok
+    {
+        string name;
+        double price;
+        int numberRemaining;
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int NumberRemaining { get; set;}
+
+    }
+}
