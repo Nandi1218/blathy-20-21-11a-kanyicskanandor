5b2ce5be3d3efe1ccf81164ee1e42b5378bc1975
Nandi1218 <kanyicskan@gmail.com>
2021-02-10 14:44:48 +0100

    Create Lista.cs

---
+++ Projektmunka/Projekt/Class/Lista.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    public class Lista
+    {
+        public Lista()
+        { 
+        
+        }
+
+        string name;
+        float price;
+        int numberRemaining;
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int NumberRemaining { get; set; }
+    
+
+    }
+}
