ef1ff9bc0e43fb6210af3559b32e4b1de44aa0d7
Nandi1218 <kanyicskan@gmail.com>
2021-02-17 18:14:42 +0100

    Update Adatok.cs

--- Projektmunka/Projekt/Class/Adatok.cs
+++ Projektmunka/Projekt/Class/Adatok.cs
@@ -8,26 +8,26 @@ namespace Projekt
 {
       public class GameData : IComparable
       {
-          public GameData(string beSor)
-          {
-              string[] darabok = beSor.Split(';');
-              name = darabok[0];
-              developer = darabok[1];
-              publisher = darabok[2];
-          }
-          string name;
-          string developer;
-          string publisher;
+            public GameData(string beSor)
+            {
+                  string[] darabok = beSor.Split(';');
+                  name = darabok[0];
+                  developer = darabok[1];
+                  publisher = darabok[2];
+            }
+            string name;
+            string developer;
+            string publisher;
           
 
-          public int CompareTo(object obj)
-          {
-              return this.name.CompareTo((obj as GameData).name);
-          }
-        public override string ToString()
-        {
-            return string.Format("{0} {1} {2}", name, developer, publisher);
-        }
-
-    }
+            public int CompareTo(object obj)
+            {
+                return this.name.CompareTo((obj as GameData).name);
+            }
+            public override string ToString()
+            {
+                return string.Format("{0} {1} {2}", name, developer, publisher);
+            }
+            
+      }
 }
